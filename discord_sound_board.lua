fdb14043a95abb89d6cdabb93979172e082e8878
Fabio Colacio <fcolacio98@gmail.com>
2017-07-18 13:55:40 -0700

    enhanced error handeling and cleans up messages

--- discord_sound_board.lua
+++ discord_sound_board.lua
@@ -58,35 +58,92 @@ function sounds_queue:isEmpty()
     end
 end
 
+function sounds_queue:clear()
+    while not self:isEmpty() do
+        self:dequeue()
+    end
+end
+
+
 client:on("ready", function()
     print("Logged in as:", client.user.username)
 end)
 
 local voice_channel = nil
 local voice_connection = nil
+local now_playing = nil
+
+local function playQueue()
+    if voice_channel and
+       voice_connection and not
+       voice_connection.isPlaying then
+        while not sounds_queue:isEmpty() do
+            local sound = sounds_queue:dequeue()
+            local sound_file = sounds_list[sound]
+            print("playing: ", sound_file)
+            now_playing = sound
+            client:setGameName(sound)
+            voice_connection:playFile(sound_file)
+            print("done playing:", sound_file)
+        end
+        
+        voice_channel:leave()
+        voice_channel = nil
+        voice_connection = nil
+        now_playing = nil
+        client:setGameName(nil)
+    end
+end
 
 client:on('messageCreate', function(message)
     if sounds_list[message.content] then
+        message:delete()
         print("queuing:", sounds_list[message.content])
-        sounds_queue:enqueue(sounds_list[message.content])
+        sounds_queue:enqueue(message.content)
         
         if not voice_connection then
             voice_channel = message.member.voiceChannel
             voice_connection = voice_channel:join()
         end
         
-        if voice_connection and not voice_connection.isPlaying then
-            while not sounds_queue:isEmpty() do
-                local sound = sounds_queue:dequeue()
-                print("playing: ", sound)
-                voice_connection:playFile(sound)
-                print("done playing:", sound)
-            end
-            
-            voice_channel:leave()
-            voice_channel = nil
-            voice_connection = nil
+        pcall(playQueue)
+    elseif message.content == "!sb_list" then
+        message:delete()
+        local sounds = "**Available Sounds**\n```\n"
+        for key, value in pairs(sounds_list) do
+            sounds = sounds .. key .. "\n" 
         end
+        sounds = sounds .. "```"
+        message.channel:sendMessage(sounds)
+    elseif message.content == "!sb_refresh" then
+        message:delete()
+        sounds_list:refresh()
+    elseif message.content == "!sb_queue" then
+        message:delete()
+        local queue = "**Queued Sounds**\n```\n" ..
+                      "Currently Playing:\n" ..
+                      (now_playing or "none") .. "\n\n" ..
+                      "Coming Up:\n"
+        if sounds_queue:isEmpty() then
+            queue = queue .. "none"
+        else
+            for index, value in ipairs(sounds_queue) do
+                queue = queue .. string.format("%d)\t%s\n", index, value)
+            end
+        end
+        queue = queue .. "```"
+        message.channel:sendMessage(queue)
+    elseif message.content == "!sb_skip" then
+        message:delete()
+        if voice_connection then
+            voice_connection:stopStream()
+        end
+    elseif message.content == "!sb_stfu" then
+        message:delete()
+        if voice_connection then
+            voice_connection :stopStream()
+        end
+        sounds_queue:clear()
     end
 end)
 
